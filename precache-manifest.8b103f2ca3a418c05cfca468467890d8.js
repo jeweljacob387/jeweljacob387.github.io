1ecec7a169aa06155a9a52ca07d89ca917f8d23a
jeweljacob387 <jeweljacob387@gmail.com>
2020-05-13 23:29:36 +0530

    Added a sample react build

---
+++ precache-manifest.8b103f2ca3a418c05cfca468467890d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ebe74d00f532079293b5a8ba50710f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b20909b4f5af81ac543",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21f2ea0dffe9a0cd7525",
+    "url": "/static/js/2.1692dd90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1692dd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b20909b4f5af81ac543",
+    "url": "/static/js/main.aebde977.chunk.js"
+  },
+  {
+    "revision": "4dc62bebe3fc9f82e06b",
+    "url": "/static/js/runtime-main.5c70ac9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
